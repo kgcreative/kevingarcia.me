117273440a7d1c1f5f20ef04c07fa0dfb78365a7
Kevin Garcia <kevin@kgcreative.com>
2018-05-28 19:30:33 -0700

    🙌🏼

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { Router, Link } from 'react-static'
 import { hot } from 'react-hot-loader'
 import Routes from 'react-static-routes'
 import NavMenu from './components/NavMenu'
+
 // These have to be imported here for css blocks to find them.
 // I'm not sure what that does to the webpack output.
-// In theory we can add a way to provide additional entry points to css blocks that aren't part of webpack.
-// or maybe there's a way to get react-static and webpack's entry points better aligned than they are by default?
+//
+// In theory we can add a way to provide additional entry points to css blocks 
+// that aren't part of webpack.
+//
+// or maybe there's a way to get react-static and webpack's entry points
+// better aligned than they are by default?
 import F04 from "./containers/404.jsx";
 import About from "./containers/About.jsx";
 import Blog from "./containers/Blog.jsx";
